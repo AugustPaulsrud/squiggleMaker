bc56ba5d28166427f7655600c355750957ebff67
August Paulsrud <augustpaulsrud@gmail.com>
2022-10-16 20:04:36 +0200

    Initial commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        //Read input
+
+        //Call lowresMaker
+
+        //Call squiggleMaker
+
+        //Done
+
+    }
+    
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# Squiggle Maker
+
+## Authors
+
+August Paulsrud 
+Victor Löfgren
+
+## What it is
+A comand line program that converts an image to squigly line art.
+
+## How to run
+TBD
