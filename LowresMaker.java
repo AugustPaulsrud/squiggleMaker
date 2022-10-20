de976d77fa28f07a5a7ad48414ca766d364c9b7f
Victor Löfgren <victor.l0fgr3n@gmail.com>
2022-10-20 14:57:04 +0200

    drawLine now handles more cases, implement drawSinus method

--- LowresMaker.java
+++ LowresMaker.java
@@ -10,36 +10,27 @@ public class LowresMaker
 
     public static void main(String[] args)  
     {
-        String fileName = args[0];
+        // String fileName = args[0];
+        // lowresMake(fileName);        
 
-        lowresMake(fileName);
+        int width = 40;
+        int height = 40;
+        int frequency = 4;
+        int backgroundColor = 0xFF000000;
+        int lineColor = 0xFFFFFFFF;
 
-        // ===== Draw line =====
-        // int x1 = 2;
-        // int y1 = 2;
-        // int x2 = 800;
-        // int y2 = 600;
-        // int width = 1000;
-        // int height = 1000;
-        // int background = 0xFF000000;
-        // int edgeColor = 0xFFFF0000;
-        // int lineColor = 0xFFFFFFFF;
+        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_4BYTE_ABGR);
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                image.setRGB(x, y, backgroundColor);
+            }
+        }
 
-        // BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_4BYTE_ABGR);
-        // for (int x = 0; x < width; x++)
-        // {
-        //     for (int y = 0; y < height; y++)
-        //     {
-        //         image.setRGB(x, y, background);
-        //     }
-        // }
+        drawSinus(image, lineColor, frequency);
 
-        // drawLineDDA(image, lineColor, x1, y1, x2, y2);
-
-        // image.setRGB(x1, y1, edgeColor);
-        // image.setRGB(x2, y2, edgeColor);
-
-        // saveImage(image, "drawline.png", "png");
+        saveImage(image, "drawline.png", "png");
     }
     
     public LowresMaker()
@@ -83,27 +74,90 @@ public class LowresMaker
         if (   (x1 < 0 || image.getWidth()  <= x2)
             || (y1 < 0 || image.getHeight() <= y2)) {
             System.out.println("Can't draw line, start or end point lies outside image");
+            System.out.printf("Image size: %dx%d\n", image.getWidth(), image.getHeight());
+            System.out.printf("(x1, y1)=(%d, %d) ", x1, y1);
+            System.out.printf("(x2, y2)=(%d, %d)", x2, y2);
             System.exit(1);
         }
 
         double m = ((double)y2 - (double)y1) / ((double)x2 - (double)x1);
 
         if (-1 <= m && m <= 1) {
-            double y = y1;
-            for (int x = x1; x < x2; x++)
+            double y = x1 < x2 ? y1 : y2;
+            int xStart = x1 < x2 ? x1 : x2;
+            int xEnd = x1 < x2 ? x2 : x1;
+                        
+            for (int x = xStart; x <= xEnd; x++)
             {
                 image.setRGB(x, (int)Math.round(y), color);
                 y += m;
             }
+
         } else {
             m = 1 / m;
-            double x = x1;
-            for (int y = y1; y < y2; y++)
+
+            double x = y1 < y2 ? x1 : x2;
+            int yStart = y1 < y2 ? y1 : y2;
+            int yEnd = y1 < y2 ? y2 : y1;
+
+            for (int y = yStart; y <= yEnd; y++)
             {
                 image.setRGB((int)Math.round(x), y, color);
                 x += m;
             }
+
         }
+    }
+
+    /**
+     * Draw the function sin(frequency*x) for x in [-pi, pi] with color. It is
+     * drawn by sampling the function at some points and drawing lines between
+     * them.
+     * 
+     * @param image the image to draw into
+     * @param color the color to draw the sinus with
+     * @param frequency the frequency of the sinus
+     */
+    public static void drawSinus(BufferedImage image, int color, double frequency)
+    {
+        double a = -Math.PI;
+        double b = Math.PI;
+
+        int width = image.getWidth();
+        int height = image.getHeight();
+
+        int n = width*4+1; // The number of points to sample
+
+        double h = (b - a) / (n - 1);
+
+        double xPrev;
+        double yPrev;
+
+        double xCurr = a;
+        double yCurr = Math.sin(frequency*a);
+
+        for (int i = 1; i < n; i++)
+        {
+            xPrev = xCurr;
+            xCurr += h;
+
+            yPrev = yCurr;
+            yCurr = Math.sin(frequency*xCurr);
+
+            // x and x are in "function" coordinates, but we want to draw it so
+            // remap them to image coordinates
+            int xImagePrev = (int) Math.round(map(a, b, 0, width-1, xPrev));
+            int yImagePrev = (int) Math.round(map(-1, 1, height-1, 0, yPrev));
+            int xImageCurr = (int) Math.round(map(a, b, 0, width-1, xCurr));
+            int yImageCurr = (int) Math.round(map(-1, 1, height-1, 0, yCurr));
+
+            drawLineDDA(image, color, xImagePrev, yImagePrev, xImageCurr, yImageCurr);
+        }
+    }
+
+    public static double map(double aOrig, double bOrig, double aTarget, double bTarget, double value)
+    {
+        return ((value-aOrig) / (bOrig-aOrig))*(bTarget-aTarget)+aTarget;
     }
 
     public static int averageCellColor(BufferedImage cell) {
